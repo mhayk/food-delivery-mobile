fba7d659862ef668ececba927d46c270cd42679c
Mhayk Whandson <hi@mhayk.com>
2024-02-11 17:56:27 +0000

    feat: finish the component category-button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,17 @@
 import { View, FlatList } from 'react-native'
-
 import { Header } from '@/components/header'
 import { CategoryButton } from '@/components/category-button'
 
 import { CATEGORIES } from '@/utils/data/products'
+import { useState } from 'react'
 
 export default function Home() {
+    const [category, setCategory] = useState(CATEGORIES[0]) // [1
+
+    function handleCategorySelected(selectedCategory: string) {
+        setCategory(selectedCategory)
+    }
+
     return (
         <View className='flex-1 pt-8'>
             <Header title="Do your order" cartQuantityItems={5} />
@@ -16,7 +22,10 @@ export default function Home() {
                 horizontal
                 showsHorizontalScrollIndicator={false}
                 renderItem={({ item }) => (
-                    <CategoryButton title={item} />
+                    <CategoryButton
+                        title={item}
+                        isSelected={category === item}
+                        onPress={() => handleCategorySelected(item)} />
                 )}
                 className='max-h-10 mt-5'
                 contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
