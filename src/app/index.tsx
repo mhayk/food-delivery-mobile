4f00238ad085afc53c9acca8e7ffe98441d068b9
Mhayk Whandson <hi@mhayk.com>
2024-02-11 17:34:09 +0000

    feat: create header component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
-import { View, Text } from 'react-native'
+import { Header } from '@/components/header'
+import { View } from 'react-native'
 
 export default function Home() {
     return (
         <View className='flex-1 pt-8'>
-            <Text className="text-white text-2xl font-heading">React Native!</Text>
+            <Header title="Do your order" cartQuantityItems={5} />
         </View>
     )
 }
