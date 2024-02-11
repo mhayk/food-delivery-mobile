7ee24eda7701de75337a5ae1e7c6133cbcefb6fa
Mhayk Whandson <hi@mhayk.com>
2024-02-11 17:47:43 +0000

    feat: create a component category-button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,26 @@
+import { View, FlatList } from 'react-native'
+
 import { Header } from '@/components/header'
-import { View } from 'react-native'
+import { CategoryButton } from '@/components/category-button'
+
+import { CATEGORIES } from '@/utils/data/products'
 
 export default function Home() {
     return (
         <View className='flex-1 pt-8'>
             <Header title="Do your order" cartQuantityItems={5} />
+
+            <FlatList
+                data={CATEGORIES}
+                keyExtractor={item => item}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                renderItem={({ item }) => (
+                    <CategoryButton title={item} />
+                )}
+                className='max-h-10 mt-5'
+                contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+            />
         </View>
     )
 }
