aaf40a2aa62e39decbfd979e011bee1060fc29ea
Mhayk Whandson <hi@mhayk.com>
2024-02-11 20:45:56 +0000

    feat: import product component and adjust the animation click

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,30 @@
-import { View, FlatList } from 'react-native'
+import { View, FlatList, SectionList, Text } from 'react-native'
 import { Header } from '@/components/header'
 import { CategoryButton } from '@/components/category-button'
 
-import { CATEGORIES } from '@/utils/data/products'
-import { useState } from 'react'
+import { CATEGORIES, MENU } from '@/utils/data/products'
+import { useRef, useState } from 'react'
+import { Product } from '@/components/product'
 
 export default function Home() {
     const [category, setCategory] = useState(CATEGORIES[0]) // [1
 
+    const sectionListRef = useRef<SectionList>(null)
+
     function handleCategorySelected(selectedCategory: string) {
         setCategory(selectedCategory)
+
+        const sectionIndex = CATEGORIES.findIndex(
+            (category) => category === selectedCategory
+        )
+
+        if (sectionListRef.current) {
+            sectionListRef.current.scrollToLocation({
+                sectionIndex,
+                itemIndex: 0,
+                animated: true
+            })
+        }
     }
 
     return (
@@ -20,7 +35,6 @@ export default function Home() {
                 data={CATEGORIES}
                 keyExtractor={item => item}
                 horizontal
-                showsHorizontalScrollIndicator={false}
                 renderItem={({ item }) => (
                     <CategoryButton
                         title={item}
@@ -28,7 +42,24 @@ export default function Home() {
                         onPress={() => handleCategorySelected(item)} />
                 )}
                 className='max-h-10 mt-5'
+                showsHorizontalScrollIndicator={false}
                 contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+            />
+
+            <SectionList
+                ref={sectionListRef}
+                sections={MENU}
+                keyExtractor={item => item.id}
+                stickySectionHeadersEnabled={false}
+                renderItem={({ item }) => <Product data={item} />}
+                renderSectionHeader={({ section: { title } }) => (
+                    <Text className='text-xl text-white font-heading mt-8 mb-3'>
+                        {title}
+                    </Text>
+                )}
+                className="flex-1 p-5"
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 100 }}
             />
         </View>
     )
