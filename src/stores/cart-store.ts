5ab0b54cbac772f784ef21f047e99310951a1832
Mhayk Whandson <hi@mhayk.com>
2024-02-11 22:19:17 +0000

    feat: persisting in the localstorage

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,5 +1,8 @@
 import { ProductProps } from '@/utils/data/products';
 import { create } from 'zustand';
+import { createJSONStorage, persist } from "zustand/middleware";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import * as cartInMemory from './helpers/cart-in-memory';
 
 export type ProductCartProps = ProductProps & {
@@ -12,14 +15,21 @@ type StateProps = {
     remove: (product: ProductProps) => void
 }
 
-export const useCartStore = create<StateProps>((set) => ({
-    products: [],
-    add: (product: ProductProps) =>
-        set((state) => ({
-            products: cartInMemory.add(state.products, product)
-        })),
-    remove: (product: ProductProps) =>
-        set((state) => ({
-            products: cartInMemory.remove(state.products, product)
-        }))
-}))
+export const useCartStore = create(
+    persist<StateProps>((set) => ({
+        products: [],
+        add: (product: ProductProps) =>
+            set((state) => ({
+                products: cartInMemory.add(state.products, product)
+            })),
+        remove: (product: ProductProps) =>
+            set((state) => ({
+                products: cartInMemory.remove(state.products, product)
+            }))
+    }),
+        {
+            name: 'food-delivery-mobile:cart',
+            storage: createJSONStorage(() => AsyncStorage)
+        }
+    )
+)
